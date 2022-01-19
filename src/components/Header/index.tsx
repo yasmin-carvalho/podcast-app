00ba8d5ee9766486307e6047d42a60158d2be39e
yasmin-carvalho <yasmin.carvalho@tech4h.com.br>
2022-01-18 22:58:42 -0300

    feat: inicilizando repositorio

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from './styles.module.scss';
+
+export function Header() {
+    return (
+        <header className={styles.headerContainer}>
+            <img src="/logo.svg" alt="Podcastr" />
+           
+            <p>O melhor para você ouvir, sempre</p>
+
+            <span>Qui, 8 Abril</span>
+        </header>
+    );
+}
