b6707eff6a1c1118f2908fe8570ee9730c9877f7
Yasmin Carvalho <yasminkarolynecarvalho@gmail.com>
2022-01-26 18:55:44 -0300

    add header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,19 @@
 /* eslint-disable @next/next/no-img-element */
 import styles from './styles.module.scss';
+import ptBR from 'date-fns/locale/pt-BR';
+import format from 'date-fns/format'
 
 export function Header() {
+    const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+        locale: ptBR,
+    })
     return (
         <header className={styles.headerContainer}>
             <img src="/logo.svg" alt="Podcastr" />
            
             <p>O melhor para você ouvir, sempre</p>
 
-            <span>Qui, 8 Abril</span>
+            <span>{currentDate}</span>
         </header>
     );
 }
